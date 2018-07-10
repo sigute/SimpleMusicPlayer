942906b82cc2c3412852e41c7969176b8f568254
Sigute <sigute.kateivaite@gmail.com>
2018-07-10 15:44:51 +0200

    added new packages

--- app/src/main/java/com/github/sigute/repobrowser/ui/SearchActivity.kt
+++ app/src/main/java/com/github/sigute/repobrowser/ui/SearchActivity.kt
@@ -1,7 +1,8 @@
-package com.github.sigute.repobrowser
+package com.github.sigute.repobrowser.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.sigute.repobrowser.R
 
 class SearchActivity : AppCompatActivity() {
 
