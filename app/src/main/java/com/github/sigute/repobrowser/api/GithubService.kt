f07cc3ae0238ca0f353dc11302940fd4dfdeae0c
Sigute <sigute.kateivaite@gmail.com>
2018-07-10 19:40:22 +0200

    Fixed the parameter names

--- app/src/main/java/com/github/sigute/repobrowser/api/GithubService.kt
+++ app/src/main/java/com/github/sigute/repobrowser/api/GithubService.kt
@@ -1,14 +1,15 @@
 package com.github.sigute.repobrowser.api
 
 import com.github.sigute.repobrowser.api.model.SearchRepositoriesResponse
+import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
     @GET("/search/repositories")
     fun searchRepositories(
-            @Path("q") query: String,
-            @Path("sort") sort: String?,
-            @Path("order") order: String = "desc")
-            : SearchRepositoriesResponse
+            @Query("q") query: String,
+            @Query("sort") sort: String?,
+            @Query("order") order: String = "desc")
+            : Single<SearchRepositoriesResponse>
 }
